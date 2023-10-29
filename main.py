7b02fc0a1325a3d8cd194f52fdaa166622668ba9
Luan Nguyen <tailuan30092000@gmail.com>
2023-10-28 22:55:56 -0700

    API 2.5 + active key

---
+++ main.py
@@ -0,0 +1,9 @@
+import argparse
+import pyfiglet
+from simple_chalk import chalk
+
+# API key for openweathermap
+API_KEY = "f67b12ec4904d1ff7c45001e43f61246"
+
+# Base on URL for openweathermap API
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
