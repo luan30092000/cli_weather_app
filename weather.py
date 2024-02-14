e115b3e98b8c59be0ae75f2f785c13ae32ff8cf0
Luan Nguyen <tailuan30092000@gmail.com>
2024-02-13 20:25:11 -0800

    Update print function and .gitignore to ignore python vm files

--- weather.py
+++ weather.py
@@ -4,6 +4,7 @@ import json
 from pprint import pp
 from configparser import ConfigParser
 from urllib import parse, request, error
+from datetime import datetime
 
 
 BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -16,7 +17,6 @@ def _get_api_key():
         [openweather]
         api_key = <API KEY>
     """
-
     config = ConfigParser()
     config.read("secrets.ini")
     return config["openweather"]["api_key"] 
@@ -31,9 +31,9 @@ def read_user_cli_args():
     parser = argparse.ArgumentParser(description="Get weather and temperature information for a city")
 
     parser.add_argument(
-        "city", 
-        nargs="+",              # need at least 1 city
-        type = str,
+        "city",                 
+        nargs="+",          # city name can have multiple words
+        type = str,         # city name is a string
         help = "City name. Format: '<City name>, <Country Code>"
     )
 
@@ -41,7 +41,14 @@ def read_user_cli_args():
         "-i",
         "--imperial",
         action = "store_true",
-        help = "change unit to imperial units"
+        help = "display the temperature in imperial units (Fahrenheit), default is metric (Celsius)",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--all",
+        action = "store_true",
+        help = "display all weather information, default is to display a pretty summary",
     )
 
     return parser.parse_args()
@@ -57,11 +64,9 @@ def build_weather_query(city_input, imperial = False):
         str: URL formatted for a call to OpenWeather's city name endpoint
     """
     api_key = _get_api_key()
-    city_name = " ".join(city_input)
-    # print(city_name)
-    url_encoded_city_name = parse.quote_plus(city_name)
-    # print(url_encoded_city_name)
-    units = "imperial" if imperial else "metric"
+    city_name = " ".join(city_input)                    # Join the list of city name words into a single string e.g. ["New", "York"] -> "New York"
+    url_encoded_city_name = parse.quote_plus(city_name) # URL encode city name e.g. "New York" -> "New+York"
+    units = "imperial" if imperial else "metric"        # Use imperial units if requested
     url = (
         f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
         f"&units={units}&appid={api_key}"
@@ -82,7 +87,7 @@ def get_weather_data(query_url):
     except error.HTTPError as http_error:
         if http_error.code == 401: # 401 - Unauthorized
             sys.exit("Access denied. Check API key.")
-        elif http_error.code == 404: # 404 - Not found
+        elif http_error.code == 404: # 404 - Not Found
             sys.exit("Cannot find weather for this city.")
         else:
             sys.exit("Something went wrong... ({http_code.code})")
@@ -93,7 +98,7 @@ def get_weather_data(query_url):
     except:
         sys.exit("Couldn't read server response.")
 
-def display_weather_info(weather_data, imperial = False):
+def display_all_weather_info(weather_data, imperial = False):
     """Prints formatted weather information about a city.
     
     Args:
@@ -101,11 +106,60 @@ def display_weather_info(weather_data, imperial = False):
         imperial (bool): Whether or not to use imperial units for temperature
         
         More information at https://openweathermap.org/current#name"""
+    
+    city = weather_data["name"]
+    country = weather_data["sys"]["country"]
+    description = weather_data["weather"][0]["description"]
+    temp = weather_data["main"]["temp"]
+    temp_min = weather_data["main"]["temp_min"]
+    temp_max = weather_data["main"]["temp_max"]
+    feels_like = weather_data["main"]["feels_like"]
+    humidity = weather_data["main"]["humidity"]
+    wind_speed = weather_data["wind"]["speed"]
+    wind_deg = weather_data["wind"]["deg"]
+    visibility = weather_data["visibility"]
+    sunrise = weather_data["sys"]["sunrise"]
+    sunrise = datetime.fromtimestamp(sunrise).strftime("%H:%M:%S")
+    sunset = weather_data["sys"]["sunset"]
+    sunset = datetime.fromtimestamp(sunset).strftime("%H:%M:%S")
 
+    print(f"{REVERSE}Weather in {city}, {country}{RESET}")
+    print(f"Status: {description}")
+    print(f"Temperature: {temp}°{'F' if imperial else 'C'}")
+    print(f"Feels like: {feels_like}°{'F' if imperial else 'C'}")
+    print(f"Min temperature: {temp_min}°{'F' if imperial else 'C'} Max temperature: {temp_max}°{'F' if imperial else 'C'}")
+    print(f"Humidity: {humidity}%")
+    print(f"Wind: {wind_speed} m/s, {wind_deg}°")
+    print(f"Visibility: {visibility} m")
+    print(f"Sunrise: {sunrise}, Sunset: {sunset}")
+
+PADDING = 20
+REVERSE = "\033[;7m"
+RESET = "\033[0m"
+
+def display_pretty_weather_info(weather_data, imperial = False):
+    city = weather_data["name"]
+    description = weather_data["weather"][0]["description"]
+    temp = weather_data["main"]["temp"]
+    sunrise = weather_data["sys"]["sunrise"]
+    sunrise = datetime.fromtimestamp(sunrise).strftime("%H:%M:%S")
+    sunset = weather_data["sys"]["sunset"]
+    sunset = datetime.fromtimestamp(sunset).strftime("%H:%M:%S")
+
+    print(f"{REVERSE}{city:^{PADDING}}{RESET}", end="")
+    print(
+        f"\t{description.capitalize():^{PADDING}}",
+        end=" ",
+    )
+    print(f"({temp}°{'F' if imperial else 'C'})")
+
+def display_weather_info(weather_data, imperial = False, all = False):
+    if all:
+        display_all_weather_info(weather_data, imperial)
+    else:
+        display_pretty_weather_info(weather_data, imperial)
 if __name__ == "__main__":
-    user_args = read_user_cli_args()
-    print(user_args)
-    query_url = build_weather_query(user_args.city, user_args.imperial)
-    print(query_url)
-    weather_data = get_weather_data(query_url)
-    pp(weather_data)
+    user_args = read_user_cli_args()    # Read user input from the command line
+    query_url = build_weather_query(user_args.city, user_args.imperial) # Build the URL for the API request
+    weather_data = get_weather_data(query_url)  # Make the API request and get the data
+    display_weather_info(weather_data, user_args.imperial, user_args.all) # Display the weather information
